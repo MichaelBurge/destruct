7db564a079a23fd9a60789afc448a0535852103a
Michael Burge <michaelburge@pobox.com>
2022-05-02 14:01:32 -0700

    Update dependencies

--- info.rkt
+++ info.rkt
@@ -3,3 +3,7 @@
 (define collection "destruct")
 (define version "1.0")
 (define scribblings '(("destruct.scrbl" ())))
+
+(define deps '("base"))
+(define build-deps '("rackunit-lib"
+                     "scribble-lib"))
